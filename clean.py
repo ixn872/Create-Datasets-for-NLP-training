97e1e817c763c662ac9a6f4fddf77098158332f7
Nicole Douglas <45563263+ixn872@users.noreply.github.com>
2020-04-25 02:00:49 +0200

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,23 +1,19 @@
 import csv
 import pandas as pd
+import codecs
 
-vocabulary=open("train_vocabulary.txt","a")
-f=open("train_source.txt","r")
-f2=open("train_target.txt","a")
+f=open('train.txt','a')
+f2=open('target.txt','a')
 
-with open('quora_questions_train.csv') as csv_file:
-    data = pd.read_csv(csv_file,header=0)
+
+with codecs.open('train.csv', mode='r', encoding='UTF-8', errors='strict', buffering=1) as csv_file:
+
+    data = pd.read_csv(csv_file,header=0,encoding='UTF-8')
     for index, row in data.iterrows():
         if row['is_duplicate']==1:
-           f.write(row['question1']+'\n')     
+           row['question1']=row['question1'].encode('ascii','ignore').decode('UTF-8','ignore')
+           row['question2']=row['question2'].encode('ascii','ignore').decode('UTF-8','ignore')
+           f.write(row['question1']+'\n')
            f2.write(row['question2']+'\n')
-
-    file=f.read()
-    word_list = file.split()
-    unique_words = set(word_list)
-    for word in unique_words:
-        vocabulary.write(str(word) + "\n")
-
-
-f.close()
-f2.close()
+    f.close()
+    f2.close()
